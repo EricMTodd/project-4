290291db295c9c3d454fdf10eff0809929e7932f
Richarmi <michael.richards@ucdenver.edu>
2018-08-17 09:51:32 -0600

    first commit- serializers and models

--- forumsite_django/srvvrs/models.py
+++ forumsite_django/srvvrs/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Thread(models.Model):
+    thread_title = models.CharField(max_length=100)
+    thread_author = models.CharField(max_length=100)
+    thread_date = models.DateTimeField(auto_now_true=True)
+
+
+    def __str__(self):
+        return self.title
+
+class Post(models.Model):
+    post_title = models.CharField(max_length=100)
+    post_author = models.CharField(max_length=255)
+    content = models.CharField(max_length=255)
+    post_date = models.DateTimeField(auto_now_true=True)
+    thread_id = models.ForeignKey(Thread, on_delete=models.CASCADE, related_name='threads')
+
+    def __str__(self):
+        return self.content
