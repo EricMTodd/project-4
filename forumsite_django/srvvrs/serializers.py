479a906bf62519423f32b1a73f82fd696499d3cb
Eric M Todd <ericmtodd@Erics-MacBook-Air.local>
2018-08-21 13:28:36 -0600

    EricaLogin integrated

--- forumsite_django/srvvrs/serializers.py
+++ forumsite_django/srvvrs/serializers.py
@@ -1,15 +1,31 @@
 from rest_framework import serializers
 from .models import Thread, Post
+from django.contrib.auth.models import User
 
 
-class ThreadSerializer(serializers.HyperlinkedModelSerializer):
+class ThreadSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Thread
-        fields = ('id', 'thread_title', 'thread_author', 'thread_date',)
+        fields = ('id', 'thread_title',
+                  'thread_author', 'thread_date', 'posts')
 
 
-class PostSerializer(serializers.HyperlinkedModelSerializer):
+class PostSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Post
-        fields = ('id', 'post_title', 'post_author',
+        fields = ('id', 'post_author',
                   'content', 'post_date', 'thread_id')
+
+
+class UserSerializer(serializers.ModelSerializer):
+    threads = serializers.PrimaryKeyRelatedField(
+        many=True, queryset=Thread.objects.all())
+
+    posts = serializers.PrimaryKeyRelatedField(
+        many=True, queryset=Post.objects.all())
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'posts', 'threads')
