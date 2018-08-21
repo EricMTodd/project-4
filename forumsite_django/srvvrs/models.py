1eb2c4cf1e611735113d2b23227682e50536ee22
ericabruenton <ercfit@gmail.com>
2018-08-21 10:12:59 -0600

    added auto date function

--- forumsite_django/srvvrs/models.py
+++ forumsite_django/srvvrs/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from datetime import datetime
 
 
 class Thread(models.Model):
     thread_title = models.CharField(max_length=100)
     thread_author = models.CharField(max_length=100)
-    thread_date = models.DateTimeField('date published')
+    thread_date = models.DateTimeField(("thread_date"), auto_now_add=True)
 
     def __str__(self):
         return self.thread_title
@@ -13,7 +14,7 @@ class Thread(models.Model):
 class Post(models.Model):
     post_author = models.CharField(max_length=100)
     content = models.CharField(max_length=255)
-    post_date = models.DateTimeField('date published')
+    post_date = models.DateTimeField(("post_date"), auto_now_add=True)
     thread_id = models.ForeignKey(
         Thread, on_delete=models.CASCADE, related_name='posts')
 
