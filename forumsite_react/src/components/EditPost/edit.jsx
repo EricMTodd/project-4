86c2a0000c8a9b33e6395ca901857239410926a6
Eric M Todd <ericmtodd@Erics-MacBook-Air.local>
2018-08-22 10:37:13 -0600

    Saving work

--- forumsite_react/src/components/EditPost/edit.jsx
+++ forumsite_react/src/components/EditPost/edit.jsx
@@ -2,22 +2,7 @@ import React, { Component } from 'react';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
 
-class EditPost extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      modal: false
-    };
-
-    this.toggle = this.toggle.bind(this);
-  }
-
-  toggle() {
-    this.setState({
-      modal: !this.state.modal
-    });
-  }
-    render(props) {
+const EditPost = (props) => {
       return (
         <div>
           <h4> Edit Post</h4>
@@ -35,7 +20,6 @@ class EditPost extends Component {
           </form>
         </div>
         )
-    }
   }
 
 export default EditPost;
